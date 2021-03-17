aacdb2f73661b20ec3f21f24d7ace85e44749811
jimy-byerley <jimy.byerley@gmail.com>
2021-03-17 13:09:27 +0100

    changed from distutils to setuptools (most recent version)

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
-    name = 'pnprint',
-    version = '1.0',
-    package_data = {
+	name = 'pnprint',
+	version = '1.0',
+	package_data = {
 		'': ['nprint.py'],
 		},
-    
-    author = 'Jimy Byerley',
-    author_email = 'jimy.byerley@gmail.com',
-    url = 'https://github.com/jimy-byerley/nprint',
-    license = "GNU LGPL v3",
-    description = "format and color serialized data strings, to make them more human readable",
+	
+	# metadata for pypi
+	author = 'Jimy Byerley',
+	author_email = 'jimy.byerley@gmail.com',
+	url = 'https://github.com/jimy-byerley/nprint',
+	license = "GNU LGPL v3",
+	description = "format and color serialized data strings, to make them more human readable",
+	long_description = open('README.md').read(),
+	long_description_content_type = 'text/markdown',
 )
